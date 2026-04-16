eed754a92f4f0b99e2c369eda9ef92237efb664b
M T Lazarus <thesi.laz23@gmail.com>
2026-04-16 11:27:11 +0530

    Added SensorReading java class

---
+++ src/main/java/org/example/SesorReading.java
@@ -0,0 +1,12 @@
+package org.example;
+
+/**
+ *
+ * @author thesangalazarus
+ */
+public class SesorReading {
+    private String id;
+    private long timestamp;
+    private double value;
+    
+}
