5343470d0efac02580a527e8b2de50f909b535c5
M T Lazarus <thesi.laz23@gmail.com>
2026-04-16 14:12:46 +0530

    Implemented constructors & getter methods for SensorReading

--- src/main/java/org/example/SensorReading.java
+++ src/main/java/org/example/SensorReading.java
@@ -6,4 +6,24 @@ public class SensorReading {
         private long timestamp;
         private double value;
 
+        public SensorReading(){
+        }
+
+        public SensorReading(String id, long timestamp, double value){
+            this.id = id;
+            this.timestamp = timestamp;
+            this.value = value;
+        }
+
+        public String getId(){
+            return id;
+        }
+
+        public long getTimestamp(){
+            return timestamp;
+        }
+
+        public double getValue(){
+            return value;
+        }
 }
