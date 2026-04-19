faaebd19fc4e1d4c3a75f32707561b52cac18119
M T Lazarus <thesi.laz23@gmail.com>
2026-04-19 17:11:10 +0530

    Added LinkedResourceNotException and mapper

---
+++ src/main/java/org/example/LinkedResourceNotException.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class LinkedResourceNotException extends RuntimeException {
+    public LinkedResourceNotFoundException(String message){
+        super(message);
+    }
+    
+}
---
+++ src/main/java/org/example/LinkedResourceNotFoundExceptionMapper.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import jakarta.ws.rs.core.*;
+import jakarta.ws.rs.ext.*;
+
+
+public class LinkedResourceNotFoundExceptionMapper {
+    
+}
